72f2ce35c1103690a41be977f66a2bd2b31b80ec
tuankcr9 <120387694+tuankcr9@users.noreply.github.com>
2022-12-14 16:13:12 +0700

    Add files via upload

--- online_music.sql
+++ online_music.sql
@@ -102,15 +102,36 @@ CREATE TABLE `music_list` (
 --
 
 INSERT INTO `music_list` (`id`, `name`, `singer_id`, `lyrics`, `description`, `vote`, `category_id`, `url`, `image`, `listens`) VALUES
-(1, 'Waiting For You', 1, 'lyric của waiting for you', 'Đây là bản nhạc hot nhất của MONO', 5, 3, NULL, '', 100),
-(2, 'Chạy Ngay Đi', 2, 'lyric của chạy ngay đi', 'Đây là một trong các bài hit của Sơn Tùng MTP', 5, 3, NULL, '', 100),
+(1, 'Waiting For You', 1, 'lyric của waiting for you', 'Đây là bản nhạc hot nhất của MONO', 5, 2, NULL, '', 100),
+(2, 'Chạy Ngay Đi', 2, 'lyric của chạy ngay đi', 'Đây là một trong các bài hit của Sơn Tùng MTP', 5, 2, NULL, '', 100),
 (3, 'Chạm khẽ tim anh một chút thôi', 3, 'lyric của ckta', 'Bài hát tình cảm nhẹ nhàng', 5, 4, NULL, '', 100),
 (4, 'Người Lạ Ơi', 4, 'lyric của người lạ ơi', 'Rap của Karik', 4, 5, NULL, '', 100),
 (5, 'Cơn Mơ Băng Giá', 5, 'Lyric của cơn mơ băng giá', 'nhạc của bằng kiều', 5, 1, NULL, '', 100),
 (6, 'Đường Tôi Chở Em Về', 6, 'lời bài dtcem', 'nhạc của buitruonglinh', 5, 2, NULL, '', 100),
 (7, 'Bên Trên Tầng Lầu', 7, 'lời bài bttl', 'Nhạc của tăngduytân', 5, 2, NULL, '', 100),
-(8, 'Khóc một cuộc tình', 8, 'Lời bài kmct', 'Nhạc của Đan Nguyên', 4, 1, NULL, '', 100);
-
+(8, 'Khóc một cuộc tình', 8, 'Lời bài kmct', 'Nhạc của Đan Nguyên', 4, 1, NULL, '', 100),
+(9, 'Chúng ta không thuộc về nhau', 2, 'lyric của ctktvn', 'Đây là một trong các bài hit của Sơn Tùng MTP', 5, 2, NULL, '', 100),
+(10,'CUA',9,'lyric của CUA','Bài rap của HIEUTHUHAI',5,5,NULL,'',100),
+(11, 'Nơi này có anh', 2, 'lyric của nnca', 'Đây là một trong các bài hit của Sơn Tùng MTP', 5, 2, NULL, '', 100),
+(12, 'Có chắc yêu là đây', 2, 'lyric của ccyld', 'Đây là một trong các bài hit của Sơn Tùng MTP', 5, 2, NULL, '', 100),
+(13, 'Đi để trở về', 10, 'lyric của đi để trở về', 'Bài hát tết của Soobin Hoàng Sơn', 5, 2, NULL, '', 100),
+(14, 'Tình đầu', 7, 'lời bài tình đầu', 'Nhạc của tăngduytân', 5, 2, NULL, '', 100),
+(15, 'Anh nhà ở đâu thê', 11, 'lời bài Anh nhà ở đâu thế', 'Bài hit của Amee', 5, 2, NULL, '', 100),
+(16, 'Trời giấu trời mang đi', 11, 'lời bài Anh tgrmđ', 'Bài hit của Amee', 5, 2, NULL, '', 100),
+(17, 'Sao anh chưa về', 11, 'lời bài Anh sao anh chưa về', 'Bài hit của Amee', 5, 2, NULL, '', 100),
+(18, 'Em là', 1, 'lyric của Em là', 'Đây là bản nhạc hot nhất của MONO', 5, 2, NULL, '', 100),
+(19, 'Lạc trôi', 2, 'lyric của lạc trôi', 'Đây là một trong các bài hit của Sơn Tùng MTP', 5, 2, NULL, '', 100),
+(20, 'Hãy trao cho anh', 2, 'lyric của hãy trao cho anh', 'Đây là một trong các bài hit của Sơn Tùng MTP', 5, 2, NULL, '', 100),
+(21,'Nghe như tình yêu',9,'lyric của nghe như tình yêu','Bài rap của HIEUTHUHAI',5,5,NULL,'',100),
+(22,'Bật nhạc lên',9,'lyric của bật nhạc lên','Bài rap của HIEUTHUHAI',5,5,NULL,'',100),
+(23, 'Nếu ngày ấy', 10, 'lyric của nếu ngày ấy', 'Bài hit của Soobin Hoàng Sơn', 5, 2, NULL, '', 100),
+(24, 'Phía sau một cô gái', 10, 'lyric của phía sau một cô gái', 'Bài hit nhất của Soobin Hoàng Sơn', 5, 2, NULL, '', 100),
+(25, 'Mình là gì của nhau', 12, 'lyric của mình là gì của nhau', 'Nhạc của Lou Hoàng', 5, 2, NULL, '', 100),
+(26, 'Bắt cóc con tim', 12, 'lyric của bắt cóc con tim', 'Nhạc của Lou Hoàng', 5, 2, NULL, '', 100),
+(27, 'Cảm giác lúc ấy sẽ ra sao', 12, 'lyric của cglasrs', 'Nhạc của Lou Hoàng', 5, 2, NULL, '', 100),
+(28, 'Cạn cả nước mắt', 4, 'lyric của cạn cả nước mắt', 'Bài rap không bao giờ biển diễn của Karik', 5, 5, NULL, '', 100),
+(29, 'Suýt nữa thì', 13, 'lyric của suýt nữa thì', 'Bài hát của Andiez', 5, 5, NULL, '', 100),
+(30, '1 phút', 13, 'lyric của 1 phút', 'Bài hát của Andiez', 5, 5, NULL, '', 100),
 -- --------------------------------------------------------
 
 --
@@ -173,8 +194,12 @@ INSERT INTO `singer` (`id`, `name`, `image`) VALUES
 (5, 'Bằng Kiều', NULL),
 (6, 'buitruonglinh', NULL),
 (7, 'Tăng Duy Tân', NULL),
-(8, 'Đan Nguyên', NULL);
-
+(8, 'Đan Nguyên', NULL),
+(9, 'HIEUTHUHAI', NULL),
+(10, 'Soobin Hoàng sơn', NULL),
+(11,'Amee',NULL),
+(12,'Lou Hoàng',NULL),
+(13,'Andiez',NULL);
 --
 -- Chỉ mục cho các bảng đã đổ
 --
