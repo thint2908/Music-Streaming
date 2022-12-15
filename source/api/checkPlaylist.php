67e85808bd3a0d7ffb5d992987c2e525683f0082
anhuynh02 <105099290+anhuynh02@users.noreply.github.com>
2022-12-15 15:01:32 +0700

    update addplaylist, deleteplaylist

--- source/api/checkPlaylist.php
+++ source/api/checkPlaylist.php
@@ -1,12 +1,16 @@
 <?php
 require_once("connection.php");
-if (isset($_POST['songName'])) {
+session_start();
+if (isset($_SESSION['userName']) && isset($_POST['songName'])) {
     $res = $db->query("
-            select * from playlist where music_id = (select id from music_list where name = '" . $_POST['songName'] . "')
-        ");
+            select * from playlist 
+            where music_id = (select id from music_list where name = '" . $_POST['songName'] . "') 
+            and user_id  =(select id from account where username = '". $_SESSION['userName'] ."')");
     if ($res->num_rows != 0) {
-        echo "false";
-    }else{
         echo "true";
+    }else{
+        echo "false";
     }
+}else{
+    echo "false";
 }
--- source/api/deletePlaylist.php
+++ source/api/deletePlaylist.php
@@ -1,15 +1,23 @@
 <?php
+session_start();
 require_once("connection.php");
-if (isset($_POST['songName'])) {
+if (isset($_SESSION['userName']) && isset($_POST['songName'])) {
     $res = $db->query("
-            select * from playlist where music_id = (select id from music_list where name = '" . $_POST['songName'] . "')
+            select * from playlist where music_id = (select id from music_list where name = '" . $_POST['songName'] . "') and user_id = (select id from account where username ='".$_SESSION['userName']."')
         ");
     if ($res->num_rows != 0) {
         $res = $db->query("
-            delete from playlist where music_id = (select id from music_list where name = '" . $_POST['songName'] . "')
-        ");
-        echo "Delect song from playlist successful";
+            delete from playlist where music_id =
+                (select id from music_list where name = '" . $_POST['songName'] . "')
+                and user_id =
+                (select id from account where username = '".$_SESSION['userName']."')
+            ");
+            echo "Remove song successful";
     }else{
-        echo "No song found";
+        echo "Song existed";
     }
+
+    
+}else{
+    echo "Chưa đăng nhập";
 }
